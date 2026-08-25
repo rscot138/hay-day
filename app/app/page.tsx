60cd4f560d3c4d156c380f6afcb0d7db8ea50e69
Ryan Scott <rscot138@gmail.com>
2026-08-24 23:38:00 -0500

    more cleanup

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import {
   AlertTriangle,
   ArrowRight,
